650bdb65f25c15ef58e92acee4bfe74b3f4d37da
irungumargaret <sairmargaret44@gmail.com>
2019-12-16 08:01:04 +0300

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,45 @@
+// Business logic
+
+// form submission function
 function getSize() {
-    var size = document.getElementById("pizzaSize").value;
-    return parseInt(size);
+  var size = document.getElementById("pizzaSize").value;
+  return parseInt(size);
 }
 function getCrust() {
-    var crust = document.getElementById("pizzaCrust").value;
-    return parseInt(crust);
+  var crust = document.getElementById("pizzaCrust").value;
+  return parseInt(crust);
 }
 function getTopping() {
-    var topping = document.getElementById("pizzaTopping").value;
-    return parseInt(topping);
+  var topping = document.getElementById("pizzaTopping").value;
+  return parseInt(topping);
 }
 function getQuantity() {
-    var number = document.getElementById("quantity").value;
-    return parseInt(number);
+  var number = document.getElementById("quantity").value;
+  return parseInt(number);
 }
 
 /* function to calculate the total amount */
 function totalAmount() {
-    var totals = (getSize() + getCrust() + getTopping()) * getQuantity();
-    alert("You have Ordered " + getQuantity("")  +  " pizza,"  +  ""  +  " amounting to ksh. "  +  (totals)  +  ""  +  " Thanks for your order welcome again.");
-    prompt("enter your location")
+  var totals = (getSize() + getCrust() + getTopping()) * getQuantity();
+  if (totals !== "") {
+    alert("Please fill out the order.");
+  } else {
+    alert(
+      "You have Ordered " +
+        getQuantity("") +
+        " pizza," +
+        "" +
+        " amounting to ksh. " +
+        totals +
+        "" +
+        " Thanks for your order welcome again."
+    );
+  }
+  // variable to get the location of the customer
+  var location = prompt("enter your location");
+  if (location === "") {
     alert("You order will be delivered shortly.Delivery fee is 100/=");
+  } else {
+    alert("Your order is being processed please pass by to pick your order.");
+  }
 }
