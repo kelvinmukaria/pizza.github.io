f7d16f427a5baedeada7b765a57fdc55d6a3c034
irungumargaret <sairmargaret44@gmail.com>
2019-12-17 17:24:38 +0300

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,46 @@
 // Business logic
-
+console.log('HI')
 // form submission function
 function getSize() {
   var size = document.getElementById("pizzaSize").value;
   return parseInt(size);
 }
+
 function getCrust() {
   var crust = document.getElementById("pizzaCrust").value;
   return parseInt(crust);
 }
+
 function getTopping() {
   var topping = document.getElementById("pizzaTopping").value;
   return parseInt(topping);
 }
+
+function getCheese() {
+  var cheese = document.getElementById("pizzaCheese").value;
+  return parseInt(cheese);
+}
+
 function getQuantity() {
   var number = document.getElementById("quantity").value;
   return parseInt(number);
+  var val0 = document.getElementById("quantity").value0;
+  return parseInt(val0);
 }
 
 /* function to calculate the total amount */
 function totalAmount() {
-  var totals = (getSize() + getCrust() + getTopping()) * getQuantity();
-   alert(
-      "You have Ordered " +
-        getQuantity("") +
-        " pizza," +
-        "" +
-        " amounting to ksh. " +
-        totals +
-        "" +
-        " Thanks for your order welcome again."
-    );
+  var totals = (getSize() + getCrust() + getTopping() + getCheese()) * getQuantity();
+  alert(
+    "You have Ordered " +
+    getQuantity("") +
+    " pizza," +
+    "" +
+    " amounting to ksh. " +
+    totals +
+    "" +
+    " Thanks for your order welcome again."
+  );
 
   // variable to get the location of the customer
   var location = prompt("enter your location");
